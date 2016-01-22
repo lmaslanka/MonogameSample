15fd510d0b67d2e235675bb4b0791bdca9c9e116
Lukasz Maslanka <lukasz.maslanka@gmail.com>
2016-01-21 21:00:09 -0500

    Initial Commit

---
+++ FirstGame/Camera.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace FirstGame
+{
+    public class Camera
+    {
+        // We need this to calculate the aspectRatio
+        // in the ProjectionMatrix property.
+        GraphicsDevice graphicsDevice;
+
+        Vector3 position = new Vector3(0, 20, 10);
+        float angle;
+
+        public Matrix ViewMatrix
+        {
+            get
+            {
+                var lookAtVector = new Vector3(0, -1, -.5f);
+                // We'll create a rotation matrix using our angle
+                var rotationMatrix = Matrix.CreateRotationZ(angle);
+                // Then we'll modify the vector using this matrix:
+                lookAtVector = Vector3.Transform(lookAtVector, rotationMatrix);
+                lookAtVector += position;
+
+                var upVector = Vector3.UnitZ;
+
+                return Matrix.CreateLookAt(
+                    position, lookAtVector, upVector);
+            }
+        }
+
+        public Matrix ProjectionMatrix
+        {
+            get
+            {
+                float fieldOfView = Microsoft.Xna.Framework.MathHelper.PiOver4;
+                float nearClipPlane = 1;
+                float farClipPlane = 200;
+                float aspectRatio = graphicsDevice.Viewport.Width / (float)graphicsDevice.Viewport.Height;
+
+                return Matrix.CreatePerspectiveFieldOfView(
+                    fieldOfView, aspectRatio, nearClipPlane, farClipPlane);
+            }
+        }
+
+        public Camera(GraphicsDevice graphicsDevice)
+        {
+            this.graphicsDevice = graphicsDevice;
+        }
+
+        public void Update(GameTime gameTime)
+        {                
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                angle += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                angle -= (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                var forwardVector = new Vector3(0, -1, 0);
+
+                var rotationMatrix = Matrix.CreateRotationZ(angle);
+                forwardVector = Vector3.Transform(forwardVector, rotationMatrix);
+
+                const float unitsPerSecond = 3;
+
+                this.position += forwardVector * unitsPerSecond *
+                    (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                var forwardVector = new Vector3(0, -1, 0);
+
+                var rotationMatrix = Matrix.CreateRotationZ(angle);
+                forwardVector = Vector3.Transform(forwardVector, rotationMatrix);
+
+                const float unitsPerSecond = 3;
+
+                this.position -= forwardVector * unitsPerSecond *
+                    (float)gameTime.ElapsedGameTime.TotalSeconds;
+            }
+        }
+    }
+}
---
+++ FirstGame/Game1.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace FirstGame
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        VertexPositionTexture[] floorVerts;
+        BasicEffect effect;
+        Texture2D checkerboardTexture;
+        Camera camera;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            floorVerts = new VertexPositionTexture[6];
+
+            floorVerts[0].Position = new Vector3(-20, -20, 0);
+            floorVerts[1].Position = new Vector3(-20, 20, 0);
+            floorVerts[2].Position = new Vector3(20, -20, 0);
+
+            floorVerts[3].Position = floorVerts[1].Position;
+            floorVerts[4].Position = new Vector3(20, 20, 0);
+            floorVerts[5].Position = floorVerts[2].Position;
+
+            int repetitions = 20;
+
+            floorVerts[0].TextureCoordinate = new Vector2(0, 0);
+            floorVerts[1].TextureCoordinate = new Vector2(0, repetitions);
+            floorVerts[2].TextureCoordinate = new Vector2(repetitions, 0);
+
+            floorVerts[3].TextureCoordinate = floorVerts[1].TextureCoordinate;
+            floorVerts[4].TextureCoordinate = new Vector2(repetitions, repetitions);
+            floorVerts[5].TextureCoordinate = floorVerts[2].TextureCoordinate;
+
+            effect = new BasicEffect(graphics.GraphicsDevice);
+
+            camera = new Camera(graphics.GraphicsDevice);
+
+            base.Initialize();
+        }
+
+        void DrawGround()
+        {
+            effect.View = camera.ViewMatrix;
+            effect.Projection = camera.ProjectionMatrix;
+
+            effect.TextureEnabled = true;
+            effect.Texture = checkerboardTexture;
+
+            foreach (var pass in effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+
+                graphics.GraphicsDevice.DrawUserPrimitives(
+                            PrimitiveType.TriangleList,
+                    floorVerts,
+                    0,
+                    2);
+            }
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            using (var stream = TitleContainer.OpenStream("Content/checkerboard.png"))
+            {
+                checkerboardTexture = Texture2D.FromStream(this.GraphicsDevice, stream);
+            }
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            camera.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            DrawGround();
+
+            base.Draw(gameTime);
+        }
+    }
+}
